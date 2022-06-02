72d63c62ef63373eff9f9cce633c66a98ff598e5
Marina Kaeufer <marishutka@yahoo.com>
2022-06-02 19:26:10 -0400

    test

---
+++ controllers/home-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+// const { Gallery, Painting } = require('../models');
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  try {
+    // const dbGalleryData = await Gallery.findAll({
+    //   include: [
+    //     {
+    //       model: Painting,
+    //       attributes: ['filename', 'description'],
+    //     },
+    //   ],
+    // });
+
+//     const galleries = dbGalleryData.map((gallery) =>
+//     gallery.get({ plain: true })
+//   );
+  // Send over the 'loggedIn' session variable to the 'homepage' template
+  res.render('homepage', {
+    // galleries,
+    // loggedIn: req.session.loggedIn,
+  });
+} catch (err) {
+  console.log(err);
+  res.status(500).json(err);
+}
+});
+
+
+module.exports = router;
