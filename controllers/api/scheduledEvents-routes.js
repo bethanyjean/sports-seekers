cdc9c4fd558bfbff2e4507244809cfec40dbf0c5
Bethany Griffin <bethanyjeanbg@gmail.com>
2022-06-05 11:01:08 -0400

    worked on routes

--- controllers/api/scheduledEvents-routes.js
+++ controllers/api/scheduledEvents-routes.js
@@ -29,6 +29,42 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+//Retrieves one event
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'date',
+        'time',
+      ],
+      include: [
+        {
+          model: sports,
+          attributes: ['name']
+        },
+        {
+          model: location,
+          attributes: ['name']
+        }
+      ] 
+})
+.then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 //Creates events
 router.post('/', (req, res) => {
     scheduledEvents.create({
@@ -42,6 +78,7 @@ router.post('/', (req, res) => {
         res.status(400).json(err);
       });
   });
+
   //Deletes event 
   router.delete('/:id', withAuth, (req, res) => {
     dbscheduledEventsdata.destroy({
